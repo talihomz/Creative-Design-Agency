baf987c88620b370f7f4347c40044d910db72f96
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-09 10:04:37 +0200

    gulp file fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var runSequence = require('run-sequence');
-var sass        = require('gulp-sass');
-const imagemin  = require('gulp-imagemin');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const runSequence = require('run-sequence');
+const sass        = require('gulp-sass');
+const image  = require('gulp-image');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('serve', () => {
 
   browserSync.init({
       server: "public"
@@ -17,31 +17,31 @@ gulp.task('serve', function() {
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("src/scss/main.scss")
+gulp.task('sass', () =>
+  gulp.src("src/scss/main.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+);
 
 // Compress images
 gulp.task('images', () =>
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('public/images'))
+  gulp.src('src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('public/images'))
 );
 
 // Browser Sync
-gulp.task('html', function(){
+gulp.task('html', () =>
   gulp.src('src/index.html')
     .pipe(gulp.dest('public'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+);
 
 // Default task
-gulp.task('default', function(callback){
+gulp.task('default', (callback) =>
   runSequence('sass',
               ['images', 'html'],
               'serve',
-              callback);
-});
+              callback)
+);
