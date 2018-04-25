65bd15cb29d69000fd767a24837cf0f51917cd44
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-25 07:40:22 +0200

    hamburger button is now functional

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector('.hamburger-icon');
+const links = document.querySelectorAll('.navbar-link');
 
 hamburger.addEventListener('click', toggleNavbar);
 
 function toggleNavbar(e) {
-  console.log(e);
+  [...links].map(link => {
+    if (link.style.display == 'none' || !link.style.display) {
+      link.style.display = 'block';
+    } else {
+      link.style.display = 'none';
+    }
+  });
 }
