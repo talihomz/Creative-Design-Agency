ddc1910747de0ea856ed3e4d3562a4d4049e3242
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-25 07:25:11 +0200

    showing hamburger icon and added javascript

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('serve', () => {
       server: "public"
   });
 
+  gulp.watch("src/js/*.js", ['js']);
   gulp.watch("src/index.html", ['html']);
   gulp.watch("src/scss/**/*.scss", ['sass']);
   gulp.watch("public/*.html").on('change', browserSync.reload);
@@ -38,10 +39,17 @@ gulp.task('html', () => {
     .pipe(browserSync.stream())
 });
 
+// Compile Javascript
+gulp.task('js', () => {
+  gulp.src('src/js/*.js')
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream())
+});
+
 // Default task
 gulp.task('default', (callback) =>
   runSequence('sass',
-              ['images', 'html'],
+              ['images', 'html', 'js'],
               'serve',
               callback)
 );
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger-icon');
+
+hamburger.addEventListener('click', toggleNavbar);
+
+function toggleNavbar(e) {
+  console.log(e);
+}
